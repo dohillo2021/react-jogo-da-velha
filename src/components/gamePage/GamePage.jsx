a0145c193dc11af114691dcc83b32fa83a840a6a
dohillo <81543122+dohillo2021@users.noreply.github.com>
2022-01-31 12:40:04 -0300

    Add files via upload

---
+++ src/components/gamePage/GamePage.jsx
@@ -0,0 +1,10 @@
+function GamePage () {
+  return(
+      <div className="container">
+      <h1>Jogo da Velha</h1>
+      <p>Criado por William Machado</p>
+      </div>
+  )
+}
+
+export default GamePage
