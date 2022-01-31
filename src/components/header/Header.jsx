6c14e607ea79db4c9b13acac82c7bf3e87024a1f
William Machado <dohillo@gmail.com>
2022-01-31 16:42:06 -0300

    Criado componente de Icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,18 @@
-
 import styles from './Header.module.css'
+
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
-
+import Icon from '../icon/Icon'
 
 
 function Header () {
     return(
         <div className={styles.header}>
             <Title>Jogo da Velha</Title>
-            <Subtitle>Criado por William Machado</Subtitle>
+            <Subtitle>Criado por William dos Santos Machado</Subtitle>
+            <div className={styles.iconContent}>
+                <Icon iconName="github" link="https://github.com/dohillo2021" />
+            </div>
         </div>
     )
 }
