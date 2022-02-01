2b231c2003c8647856935c460df4775272189b57
William Machado <dohillo@gmail.com>
2022-02-01 16:23:41 -0300

    JF tudo certo

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
@@ -20,27 +19,3 @@ function Game() {
 }
 
 export default Game
-=======
-import styles from './Game.module.css'
-
-import GameOption from '../gameOption/GameOption'
-
-function Game () {
-  return (
-    <div className={styles.game} >
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-    </div>
-  )
-
-}
-
-export default Game
->>>>>>> 1ae03e1544386240682d967b9d3b04d6e8a4d481
