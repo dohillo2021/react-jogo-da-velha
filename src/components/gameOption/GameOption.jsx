a2fecbc6b1fb66e0d52042de0f0479bfbf75e65a
William Machado <dohillo@gmail.com>
2022-02-01 17:18:42 -0300

    GameOption na JF 01.02.2022

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import styles from './GameOption.module.css'
-
-function GameOption (  ) {
-    return(
-        <div>
-            x
-        </div>
-    )
-}
-
-=======
 import styles from './GameOption.module.css'
 
 function GameOption () {
@@ -20,5 +8,4 @@ function GameOption () {
   )
 }
 
->>>>>>> 1ae03e1544386240682d967b9d3b04d6e8a4d481
 export default GameOption
