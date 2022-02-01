0f4a8d8ec469c74565c5ca78ae5c01f58d7b2a0c
dohillo <81543122+dohillo2021@users.noreply.github.com>
2022-02-01 18:22:45 -0300

    Add files via upload

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,10 +1,28 @@
+<<<<<<< HEAD
+import { useState } from 'react'
+
+=======
+>>>>>>> a2fecbc6b1fb66e0d52042de0f0479bfbf75e65a
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
 
+
 function Game() {
+    const [gameState, setGameState] = useState(Array(9).fill(0))
+    console.log(gameState)
     return(
         <div className={styles.game}>
+<<<<<<< HEAD
+            {
+                gameState.map((value, pos) => 
+                <GameOption
+                    key={ ` game-option-pos-${pos} `}
+                    status={value}
+                />)
+            }
+            
+=======
             <GameOption />
             <GameOption />
             <GameOption />
@@ -14,6 +32,7 @@ function Game() {
             <GameOption />
             <GameOption />
             <GameOption />
+>>>>>>> a2fecbc6b1fb66e0d52042de0f0479bfbf75e65a
         </div>
     )
 }
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,22 @@
 import styles from './GameOption.module.css'
+<<<<<<< HEAD
+import Icon from '../icon/Icon'
 
-function GameOption () {
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px"/>
+
+=======
+>>>>>>> a2fecbc6b1fb66e0d52042de0f0479bfbf75e65a
+
+function GameOption ({status}) {
   return (
     <div className={styles.gameOption}>
-      
+      {
+        status === 1 && <GameIcon iconName="circle" />
+      }
+
+      {
+        status === -1 && <GameIcon iconName="X" />
+      }    
     </div>
   )
 }
