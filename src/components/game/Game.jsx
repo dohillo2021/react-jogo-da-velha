950bb4d37f2dab900ef2d882aa2575c4895e3238
William Machado <dohillo@gmail.com>
2022-02-01 15:56:58 -0300

    Commit  JF 01.02.2021

---
+++ src/components/game/Game.jsx
@@ -0,0 +1,21 @@
+import styles from './Game.module.css'
+
+import GameOption from '../gameOption/GameOption'
+
+function Game() {
+    return(
+        <div>
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+        </div>
+    )
+}
+
+export default Game
