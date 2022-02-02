4d771ea7a6fb6acd8efc241aea4b1222caecfbf4
William Machado <dohillo@gmail.com>
2022-02-02 18:01:49 -0300

    Jogo quase pronto

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,13 +1,12 @@
 import styles from './GameOption.module.css'
-
 import Icon from '../icon/Icon'
 
 const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px"/>
 
 
-function GameOption ({status}) {
+function GameOption ({status, onClick}) {
   return (
-    <div className={styles.gameOption}>
+    <div className={styles.gameOption} onClick={onClick}>
       {
         status === 1 && <GameIcon iconName="circle" />
       }
