9dea1c52698124cf2930eeb81976548fb0e85883
William Machado <dohillo@gmail.com>
2022-02-10 15:30:34 -0300

    Criado Script de biuld e start

---
+++ src/components/gameInfo/GameInfo.jsx
@@ -0,0 +1,51 @@
+import styles from './GameInfo.module.css'
+import Icon from '../icon/Icon'
+import Button from '../button/Button'
+
+
+function GameInfo ({ currentPlayer, winner, onReset}) {
+    return (
+        <div className= { styles.gameInfo}>
+            {
+                winner === 0 &&
+                    <>
+                        <h4>Próximo a jogar:</h4> 
+                            {
+                                currentPlayer === 1 && <Icon iconName="circle" />
+                            }
+                            {
+                                currentPlayer === -1 && <Icon iconName="x" />
+                            }
+                    
+                    
+                    </>
+            }
+            {
+                winner !== 0 &&
+                    <>
+                        <h4>Fim de Jogo! Campeão:  </h4>
+                            
+                        {
+                            winner === 1 && <Icon iconName="circle" />
+                        }
+                        {
+                            winner === -1 && <Icon iconName="x" />
+                        }
+                        
+                            
+                        
+                    </>
+            }
+            <Button
+                onClick={onReset}
+            >
+                Reiniciar
+            </Button>
+          
+     </div>        
+    )
+
+}
+
+
+export default GameInfo
